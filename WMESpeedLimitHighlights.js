89180f746ea7f33e5cc3668402a839c63f915e25
jackohug <jackohug@users.noreply.github.com>
2015-11-08 18:24:24 +0000

    Create WMESpeedLimitHighlights.js

---
+++ WMESpeedLimitHighlights.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name                WME Speed Limit Highlights
+// @namespace           https://github.com/jackohug/WME-Scripts
+// @version             0.1
+// @author              jackohug
+// @description         Adds colours to road segments to show their speed limit
+// @include             https://www.waze.com/editor/*
+// @include             https://www.waze.com/*/editor/*
+// @include             https://editor-beta.waze.com/*
+// @grant               none
+// ==/UserScript==
+
